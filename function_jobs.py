4f3130056beaa833b3d7944227cc37ff02664190
dorxoc <dorxoc@gmail.com>
2020-03-29 23:21:02 -0600

    funcion ya mejorada

--- function_jobs.py
+++ function_jobs.py
@@ -6,14 +6,14 @@ def salary(value):
     splitted = value.split('-')
     if len(splitted) == 2:
         try:
-            avg = sum(list(map(float, splitted)))/2
+            avg = sum(map(float, splitted))/2
             return avg
         except ValueError:
-            return False
+            return 0.0
     else:
         try:
             return float(value)
         except ValueError:
-            return False
-        return float(value)
+            return 0.0
+        
     
