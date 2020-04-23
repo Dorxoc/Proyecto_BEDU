0c0f0653962730eaa7adfefadc99f88265372977
Dorian Xochitl <dorxoc@gmail.com>
2020-04-22 23:31:18 -0500

    hola

--- function_jobs.py
+++ function_jobs.py
@@ -1,3 +1,10 @@
+'''
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(jobs['country'].value_counts().sort_values('index'))
+'''
+
+
+
 
 '''funcion para obtener un salario promedio 
 a partir del rango de salarios'''
@@ -30,25 +37,66 @@ def country(string):
         return list_string[0] 
     else: 
         return 'unknown' 
+
     
-  
+    
+    
+    
 def state(string): 
     list_string = string.split(', ') 
     if len(list_string) == 3 and list_string[1]!='': 
         return list_string[1].upper() 
     else: 
         return 'unknown' 
- 
-
+    
+    
+    
 def city(string): 
     list_string = string.split(', ') 
     if len(list_string) == 3 and list_string[2]!='': 
         return list_string[2].lower() 
     else: 
         return 'unknown' 
+
+    
+    
+''' 
+lo que tenga que ver con vocational lo pasa a professional
+''' 
+
+def education(x):
+    if x == 'Vocational' or x == 'Vocational - Degree' or x == 'Vocational - HS Diploma' :
+        return 'Professional'
+    else:
+        return x
     
 
+'''
+conteo de palabras en un string
+'''
+def count_(string):
+    if type(string) == str:
+        
+        for i in '(,_.)/:;-1234567890':
+    
+            string = string.replace(i, '')
+     
+        list_string = string.split(' ')
+        return len(list_string)
+    else:
+        return str(string)
 
+    
 '''
-conteo
+quita todo tipo de signo posible al string
 '''
+def text(string):
+    if type(string) == str:
+        
+        for i in '(,_.)/:;-1234567890':
+    
+            string = string.replace(i, '')
+        return string
+    else:
+        str(string)
+        
